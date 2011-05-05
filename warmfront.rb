49d8cc8e9ad1e49b86533e29bd7f6dafdab1fd89
Chris May <chrismay50@gmail.com>
2011-05-05 23:08:36 +0100

    dns cache (not working yet)

--- warmfront.rb
+++ warmfront.rb
@@ -2,18 +2,30 @@
 require 'uri'
 require 'resolv'
 require 'net/http'
-
 # This bit will be extracted out into a config file
-dns=["8.8.8.8","8.8.4.4"]
+dns_servers=["8.8.8.8","8.8.4.4"]
 cloudfront_urls=[
   URI("http://d36jn9qou1tztq.cloudfront.net/static_war/render/css/packed/pack5.css.373145863781"),
   URI("http://d36jn9qou1tztq.cloudfront.net/static_war/render/css/packed/noaccessibility.css.426842630073"),
   URI("http://dwfwn5ehtp58w.cloudfront.net/static_war/render/css/packed/noaccessibility.css.426842630073")
 ]
 
-def dns_resolve(nameserver,host)
-  Resolv::DNS.open({:nameserver=>nameserver}){|r| r.getaddresses(host).collect{|ip| ip.to_s}}
+class Dns
+  def initialize
+     @dns_cache=Hash.new{|hash,key| hash[key]=Hash.new}
+  end
+  def resolve(nameserver,host)
+    cached_entry= @dns_cache[nameserver][host] 
+    if (cached_entry.empty?) then
+      cached_entry = Resolv::DNS.open({:nameserver=>nameserver}){|r| r.getaddresses(host).collect{|ip| ip.to_s}}
+      puts "looked up #{host} at #{nameserver}"
+      @dns_cache[nameserver][host] = cached_entry
+    end
+    cached_entry
+  end
 end
+
+dns=Dns.new
 
 # get the unique list of hostnames. 
 #
@@ -21,7 +33,7 @@ hostnames = cloudfront_urls.collect{ |url| url.host}.uniq
 
 # Make a list of lists of hostname=>IP
 hosts_to_ips = hostnames.collect{ |host|
-  [host,dns.collect{|server| dns_resolve(server,host) }.flatten ]
+  [host,dns_servers.collect{|server| dns.resolve(server,host) }.flatten ]
 }
 
 #Make the cross-product of IPs and URLs
