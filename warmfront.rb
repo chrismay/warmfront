84d2dceae33707456a2c946d8bca8b3c379fae2e
Chris May <chrismay50@gmail.com>
2011-05-05 22:18:04 +0100

    clean up the code a bit

--- warmfront.rb
+++ warmfront.rb
@@ -4,49 +4,37 @@ require 'resolv'
 require 'net/http'
 
 # This bit will be extracted out into a config file
-dns=["137.205.205.80","8.8.8.8","8.8.4.4"]
+dns=["8.8.8.8","8.8.4.4"]
 cloudfront_urls=[
   URI("http://d36jn9qou1tztq.cloudfront.net/static_war/render/css/packed/pack5.css.373145863781"),
-  URI("http://d36jn9qou1tztq.cloudfront.net/static_war/render/css/packed/noaccessibility.css.426842630073")
+  URI("http://d36jn9qou1tztq.cloudfront.net/static_war/render/css/packed/noaccessibility.css.426842630073"),
+  URI("http://dwfwn5ehtp58w.cloudfront.net/static_war/render/css/packed/noaccessibility.css.426842630073")
 ]
 
-# get the unique list of hostnames. Normally only one
+def dns_resolve(nameserver,host)
+  Resolv::DNS.open({:nameserver=>nameserver}){|r| r.getaddresses(host).collect{|ip| ip.to_s}}
+end
+
+# get the unique list of hostnames. 
 #
-hostnames = cloudfront_urls.map do |url|
-  url.host
-end.uniq
+hostnames = cloudfront_urls.collect{ |url| url.host}.uniq
 
-# Make a hash of hostname=>IP
-hosts_to_ips = Hash.new
-hostnames.each do |host|
-  addresses=[]
-  dns.each do |server| 
-    Resolv::DNS.open({:nameserver=>server}) do |r|
-      addresses += r.getaddresses(host)
-    end
-  end
-  hosts_to_ips[host] = addresses.uniq
-end
+# Make a list of lists of hostname=>IP
+hosts_to_ips = hostnames.collect{ |host|
+  [host,dns.collect{|server| dns_resolve(server,host) }.flatten ]
+}
 
-#Make a hash of IP=>URL to be fetched from that IP
-ips_to_urls = Hash.new(Array.new)
-cloudfront_urls.each do |url|
-  puts "fetching #{url}"
-  hosts_to_ips[url.host].each do |ip|
-    ips_to_urls[ip] +=[url]
-  end
-end
+#Make the cross-product of IPs and URLs
+ips_to_urls = cloudfront_urls.collect { |url|
+  (host,ips) = hosts_to_ips.find{|(host,ips)| host == url.host }
+  ips.collect{ |ip| [ip,url]}
+}.flatten(1)
 
-# Fetch all the URLs for all of the IPs. 
-#
-ips_to_urls.each_key do |ip|
-    puts "from #{ip} request..."
-    ips_to_urls[ip].each do |url|
-      Net::HTTP::start(ip.to_s,80)do |http|
-         headers={"Host"=>url.host}
-         res = http.get(url.path,headers )
-         puts "#{url} #{res}"
-      end
-    end
-end
+## Fetch all the URLs for all of the IPs. 
+##
+results= ips_to_urls.collect{|(ip,url)|
+    [url, Net::HTTP::start(ip,80){ |http| http.get(url.path,{"Host"=>url.host}).code} ]
+}
+
+p results
 
